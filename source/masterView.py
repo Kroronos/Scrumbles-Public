d783d3977b2953842b5bc0c8c6b91e9b65d7cd88
Jesse Martinez <kroronos@ufl.edu>
2018-02-22 16:22:28 -0500

    Exit Bug Fix
    On exit closes all windows.
    #55

--- source/masterView.py
+++ source/masterView.py
@@ -56,7 +56,7 @@ class masterView(tk.Tk):
         fileMenu = tk.Menu(menuBar, tearoff=0)
         fileMenu.add_command(label="Create New User", command=self.showCreateUserDialog)
         fileMenu.add_command(label="Create New Sprint", command=self.showCreateSprintDialog)
-        fileMenu.add_command(label="Exit", command=exitProgram)
+        fileMenu.add_command(label="Exit", command=lambda:exitProgram(self))
 
         profileMenu = tk.Menu(menuBar, tearoff=0)
         profileMenu.add_command(label="Log Out", command=lambda: logOut(self))
@@ -99,7 +99,7 @@ class masterView(tk.Tk):
         self.show_frame(mainView)
 
     def setDatabaseConnection(self):
-        dbLoginInfo = ScrumblesData.DataBaseLoginInfo()
+        dbLoginInfo = ScrumblesData.DataBaseLoginInfo("login.txt")
         dbLoginInfo.userID = 'test_user'
         dbLoginInfo.password = 'testPassword'
         dbLoginInfo.ipaddress = '173.230.136.241'
@@ -114,7 +114,8 @@ def logOut(controller):
     controller.add_frame(loginFrame, loginView)
     controller.show_frame(loginView)
 
-def exitProgram():
+def exitProgram(mainwindow):
+    mainwindow.destroy()
     print("Exiting Program")
     exit()
 
