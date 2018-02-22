d7693b3690e967e0ae7bf1f1c6633e6ca650803d
fallinbryan <fallinbryan@ufl.edu>
2018-02-21 21:08:09 -0600

    add # to visually split branches on GitHub Network View

--- source/Scrumbles.py
+++ source/Scrumbles.py
@@ -18,6 +18,7 @@ assert tkinter.TkVersion == 8.6, 'Version requirement tkinter version 8.6'
 #
 # if 'win' in sys.platform:
 #         ctypes.windll.shcore.SetProcessDpiAwareness(1)
+#
 
 app = masterView.masterView()
 
