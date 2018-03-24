d9ca88d94b83b80c99ca1269c2a762cbf3f3362f
fallinbryan <fallinbryan@ufl.edu>
2018-03-24 16:24:41 -0500

    add syncTables.py in test folder
    Fix duplicating data bug
    
    Signed-off-by: fallinbryan <fallinbryan@ufl.edu>

---
+++ source/testing/syncTables.py
@@ -0,0 +1,21 @@
+import ScrumblesData
+
+
+db = ScrumblesData.DataBlock()
+db.conn.connect()
+db.conn.setData('DELETE FROM ProjectItemTable')
+db.conn.setData('DELETE FROM ProjectUserTable')
+db.conn.close()
+
+
+#if an item is assigned to a sprint, the item should be
+#assigned to the project the sprint is assigned to
+for P in db.projects:
+    for S in P.listOfAssignedSprints:
+        for I in S.listOfAssignedItems:
+
+            db.addItemToProject(P,I)
+        for U in S.listOfAssignedUsers:
+            db.addUserToProject(P,U)
+
+db.shutdown()
