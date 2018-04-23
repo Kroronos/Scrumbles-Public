0e7a30ef6c2bc5415c0c5202e30cd6d4717cd818
Kyle Collins <kcollins7@bellsouth.net>
2018-04-22 21:50:10 -0400

    Formatted User Dialog Box

--- source/frames/UserDialogs.py
+++ source/frames/UserDialogs.py
@@ -8,42 +8,41 @@ class CreateUserDialog(GenericDialog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
         if not self.isTest:
-            self.geometry('%dx%d'%(600*self.master.w_rat, 500*self.master.h_rat))
+            self.geometry('%dx%d'%(200*self.master.w_rat, 140*self.master.h_rat))
         self.title('Create a New User')
         self.createWidgets()
 
     @tryExcept
     def createWidgets(self):
 
-        Tk.Label(self, text="User Name").grid(row=2, column=1, pady=5, sticky='E')
-        self.userNameEntry = Tk.Entry(self, cursor = "hand2")
-        self.userNameEntry.grid(row=2,column=2,pady=5)
+        Tk.Label(self, text="User Name").grid(row=2, column=1, padx=5, pady=5, sticky='W')
+        self.userNameEntry = Tk.Entry(self, width=27, cursor = "hand2")
+        self.userNameEntry.grid(row=2, column=2, pady=5, sticky='W')
 
-        Tk.Label(self, text="User Password").grid(row=3, column=1, pady=5, sticky='E')
-        self.passwordEntry = Tk.Entry(self,show='*', cursor = "hand2")
-        self.passwordEntry.grid(row=3,column=2,pady=5)
+        Tk.Label(self, text="User Password").grid(row=3, column=1, padx=5, pady=5, sticky='W')
+        self.passwordEntry = Tk.Entry(self, width=27, show='*', cursor = "hand2")
+        self.passwordEntry.grid(row=3, column=2, pady=5, sticky='W')
 
-        Tk.Label(self, text="Re-enter Password").grid(row=4, column=1, pady=5, sticky='E')
-        self.reEnterPasswordEntry = Tk.Entry(self,show='*', cursor = "hand2")
-        self.reEnterPasswordEntry.grid(row=4,column=2,pady=5)
+        Tk.Label(self, text="Re-enter Password").grid(row=4, column=1, padx=5, pady=5, sticky='W')
+        self.reEnterPasswordEntry = Tk.Entry(self, width=27, show='*', cursor = "hand2")
+        self.reEnterPasswordEntry.grid(row=4, column=2, pady=5, sticky='W')
 
-        Tk.Label(self, text="User Email Address").grid(row=5, column=1, pady=5, sticky='E')
-        self.emailEntry =Tk.Entry(self, cursor = "hand2")
-        self.emailEntry.grid(row=5,column=2,pady=5)
+        Tk.Label(self, text="User Email Address").grid(row=5, column=1, padx=5, pady=5, sticky='W')
+        self.emailEntry =Tk.Entry(self, width=27, cursor = "hand2")
+        self.emailEntry.grid(row=5, column=2, pady=5, sticky='W')
 
-        Tk.Label(self, text="User Role").grid(row=6, column=1, pady=5, sticky='E')
+        Tk.Label(self, text="User Role").grid(row=6, column=1, padx=5, pady=5, sticky='W')
         roleVar = Tk.StringVar()
         items = ('Admin', 'Scrum Master', 'Developer')
-        self.roleCombobox = ttk.Combobox(self,textvariable=roleVar,state='readonly',values=items, cursor = "hand2")
-        self.roleCombobox.grid(row=6, column=2)
+        self.roleCombobox = ttk.Combobox(self, textvariable=roleVar, state='readonly', values=items, width=24, cursor="hand2")
+        self.roleCombobox.grid(row=6, column=2, sticky='W')
         self.roleCombobox.selection_clear()
 
+        self.createButton = Tk.Button(self, text="Create User", command=self.ok, cursor="hand2")
+        self.createButton.grid(row=8, column=1, columnspan=2, padx=70, pady=5, sticky='W')
 
-        self.createButton = Tk.Button(self, text="Create User", command=self.ok, cursor = "hand2")
-        self.createButton.grid(row=8,column=2,pady=5)
-
-        self.cancelButton = Tk.Button(self, text="Cancel", command=self.exit, cursor = "hand2")
-        self.cancelButton.grid(row=8,column=1,pady=5)
+        self.cancelButton = Tk.Button(self, text="Cancel", command=self.exit, cursor="hand2")
+        self.cancelButton.grid(row=8, column=2, columnspan=2, padx=70, pady=5, sticky='W')
 
 
     @staticmethod
@@ -55,7 +54,7 @@ class CreateUserDialog(GenericDialog):
     @tryExcept
     def ok(self):
 
-        self.validatePasswordMatch(self.passwordEntry.get(),self.reEnterPasswordEntry.get())
+        self.validatePasswordMatch(self.passwordEntry.get(), self.reEnterPasswordEntry.get())
 
         user = ScrumblesObjects.User()
         user.userName = self.userNameEntry.get()
